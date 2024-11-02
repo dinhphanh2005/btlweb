a8cb50f9cfe98118c564c8efba643091b76e78b4
Dinh Phanh <89905612+dinhphanh2005@users.noreply.github.com>
2024-11-02 10:45:20 +0700

    Delete btl directory

--- btl/js/script_m_revenue.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch('/data/db_revenue.json')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("revenue-amount").textContent = data.totalRevenue.toLocaleString() + " VND";
-        })
-        .catch(error => console.error("Error loading revenue data:", error));
-});
